a2cbe95cd0007e5c274832f0384859eaf039574d
CodePeaceful <christian.schlecht@arcor.de>
2025-05-13 12:54:59 +0200

    kunde done

--- MusterloesungSchwer/Program.cs
+++ MusterloesungSchwer/Program.cs
@@ -3,4 +3,7 @@
 // parsen der Kunden Zeilen und Index hinzufügen
 var kundenprep = Enumerable.Zip(Enumerable.Range(0, kundentxt.Count() - 1), kundentxt.Skip(1).Select(Transformations.ReturnMatchesKunde));
 var kundeOrtList = kundenprep.Select(s => new KundeOrt { KID = s.First, ort = new WohnortNOid { Ort = s.Second.Ort, PLZ = s.Second.PLZ, Strasse = s.Second.Strasse, Hausnummer = s.Second.Hausnummer } });
-
+var ortListprep = Enumerable.Zip(Enumerable.Range(0, kundentxt.Count() - 1), kundeOrtList.Select(s => s.ort).Distinct());
+var ortList = ortListprep.Select(s => new DBWohnort { OID = s.First, Hausnummer = s.Second.Hausnummer, Ort = s.Second.Ort, PLZ = s.Second.PLZ, Strasse = s.Second.Strasse });
+var kundeortInts = kundeOrtList.Join(ortListprep, kunde => kunde.ort, ort => ort.Second, (kunde, ort) => new { kunde.KID, ort.First });
+var kundeList = kundenprep.Select(k => new DBKunde { KID = k.First, E_Mail = k.Second.E_Mail, Nachname = k.Second.Nachname, Passwort = k.Second.Passwort, Vorname = k.Second.Vorname, OID = kundeortInts.Single(o => o.KID == k.First).First });
