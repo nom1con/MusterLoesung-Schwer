47a9465e135c24432ac7a1c81ad20b54a5d0ac91
CodePeaceful <christian.schlecht@arcor.de>
2025-05-14 15:13:05 +0200

    improved performance

--- MusterloesungSchwer/Program.cs
+++ MusterloesungSchwer/Program.cs
@@ -1,19 +1,19 @@
 ﻿var kundentxt = File.ReadAllLines("kunden.csv");
 
 // parsen der Kunden Zeilen und Index hinzufügen
-var kundenprep = Enumerable.Zip(Enumerable.Range(1, kundentxt.Length - 1), kundentxt.Skip(1).Select(Transformations.ReturnMatchesKunde));
-var kundeOrtList = kundenprep.Select(s => new KundeOrt { KID = s.First, ort = new WohnortNOid { Ort = s.Second.Ort, PLZ = s.Second.PLZ, Strasse = s.Second.Strasse, Hausnummer = s.Second.Hausnummer } });
-var ortListprep = Enumerable.Zip(Enumerable.Range(1, kundentxt.Length - 1), kundeOrtList.Select(s => s.ort).Distinct());
+var kundenprep = Enumerable.Zip(Enumerable.Range(1, kundentxt.Length - 1), kundentxt.Skip(1).Select(Transformations.ReturnMatchesKunde)).ToArray();
+var kundeOrtList = kundenprep.Select(s => new KundeOrt { KID = s.First, ort = new WohnortNOid { Ort = s.Second.Ort, PLZ = s.Second.PLZ, Strasse = s.Second.Strasse, Hausnummer = s.Second.Hausnummer } }).ToArray();
+var ortListprep = Enumerable.Zip(Enumerable.Range(1, kundentxt.Length - 1), kundeOrtList.Select(s => s.ort).Distinct()).ToArray();
 var ortList = ortListprep.Select(s => new DBWohnort { OID = s.First, Hausnummer = s.Second.Hausnummer, Ort = s.Second.Ort, PLZ = s.Second.PLZ, Strasse = s.Second.Strasse });
 Database.StoreOrte(ortList);
 
-var kundeortInts = kundeOrtList.Join(ortListprep, kunde => kunde.ort, ort => ort.Second, (kunde, ort) => new { kunde.KID, ort.First });
+var kundeortInts = kundeOrtList.Join(ortListprep, kunde => kunde.ort, ort => ort.Second, (kunde, ort) => new { kunde.KID, ort.First }).ToArray();
 var kundeList = kundenprep.Select(k => new DBKunde { KID = k.First, E_Mail = k.Second.E_Mail, Nachname = k.Second.Nachname, Passwort = k.Second.Passwort, Vorname = k.Second.Vorname, OID = kundeortInts.Single(o => o.KID == k.First).First });
 Database.StoreKunden(kundeList);
 
 var produktetxt = File.ReadAllLines("produkte.csv");
 var produkteprep = Enumerable.Zip(Enumerable.Range(1, produktetxt.Length - 1), produktetxt.Skip(1).Select(Transformations.ReturnMatchesProdukt));
-var produkte = produkteprep.Select(p => Transformations.DBProdukt(p.First, p.Second));
+var produkte = produkteprep.Select(p => Transformations.DBProdukt(p.First, p.Second)).ToArray();
 Database.StoreProdukte(produkte);
 
 var bestellungenprep = kundenprep.Select(k => new KundenBestellungen { KID = k.First, bestellungen = Transformations.Bestellung(k.Second.Bestellposten) });
